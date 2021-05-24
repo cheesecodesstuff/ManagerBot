3402ec74f4a6386e54022c4ab4bd9db89863f959
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-05-24 22:58:32 +0530

    Update testing.py

--- manager/testing.py
+++ manager/testing.py
@@ -1,6 +1,7 @@
 from .core import _cog_check, _request, ServerEnum, Status
 from redbot.core import commands
-from discord import Embed
+from discord import Embed, Member, Color
+from http import HTTPStatus
 
 class BotTesting(commands.Cog):
     def __init__(self, bot):
@@ -9,10 +10,10 @@ class BotTesting(commands.Cog):
     async def cog_check(self, ctx):
         return await _cog_check(ctx, self.bot, ServerEnum.TEST_SERVER)
 
-    @commands.command()
+    @commands.command(aliases=["q", "pending"])
     async def queue(self, ctx):
         """Get all bots in queue"""
-        queue_json = await _request("GET", ctx, self.bot, "/api/bots/admin/queue")
+        queue_json = await _request("GET", ctx, self.bot, "/api/bots/admin/queue")[1]
         embed = Embed(title = "Bots In Queue", description = "These are the bots in the Fates List Queue. Be sure to review them from top to bottom, ignoring Fates List bots")
         i = 1
         for bot in queue_json["bots"]:
@@ -22,3 +23,17 @@ class BotTesting(commands.Cog):
         embed.set_thumbnail(url = str(ctx.guild.icon_url))
         return await ctx.send(embed = embed)
 
+    @commands.command()
+    async def claim(self, ctx, bot: Member):
+        """Claims a bot. This requires you to be staff and is checked on our API"""
+        if not bot.bot:
+            await ctx.send("That isn't a bot. Please make sure you are pinging a bot")
+            return
+        claim_res = await _reauest("PATCH", ctx, self.bot, f"/api/bots/admin/{bot.id}/under_review", json = {"mod": str(ctx.author.id), "requeue": False})
+        if not claim_res[1]["done"]:
+            embed = Embed(title = "Claim Failed" description = f"This bot could not be claimed by you...", color = Color.red())
+            embed.add_field(name = "Reason", value = claim_res[1]["reason"])
+            embed.add_field(name = "Status Code", value = f"{claim_res[0]} ({HTTPStatus(claim_res[0]).phrase}")
+            await ctx.send(embed = embed)
+        embed = Embed(title = "Claimed", description = "This bot has been claimed. Use +unclaim when you don't want it anymore")
+        await ctx.send(embed = embed)
