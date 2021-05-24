e0d2c3d34970495ae41f846679361d2313d0ec82
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-05-24 22:31:41 +0530

    Update testing.py

--- manager/testing.py
+++ manager/testing.py
@@ -16,7 +16,7 @@ class BotTesting(commands.Cog):
         embed = Embed(title = "Bots In Queue", description = "These are the bots in the Fates List Queue. Be sure to review them from top to bottom, ignoring Fates List bots")
         i = 1
         for bot in queue_json["bots"]:
-            embed.add_field(name = f"{i}. {bot['user']['username']}#{bot['user']['disc']} ({bot['user']['id']})", value = f"This bot has a status of **{Status(bot['user']['status']).__doc__}** and a prefix of **{bot['prefix']}** -> [Invite Bot]({bot['invite']})\n\n{bot['description']}")
+            embed.add_field(name = f"{i}. {bot['user']['username']}#{bot['user']['disc']} ({bot['user']['id']})", value = f"This bot has a status of **{Status(bot['user']['status']).__doc__}** and a prefix of **{bot['prefix']}** -> [Invite Bot]({bot['invite']})\n\n**Description:** {bot['description']}\n")
             i += 1
         embed.set_thumbnail(url = str(ctx.guild.icon_url))
         return await ctx.send(embed = embed)
