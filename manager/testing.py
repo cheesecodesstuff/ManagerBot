e05f46ebdcee85600f0376e108e1d1c368592c86
root <cheesycod@outlook.com>
2021-05-24 03:57:08 +0000

    Fix

--- manager/testing.py
+++ manager/testing.py
@@ -2,6 +2,9 @@ from .core import _is_staff, _cog_check, _request, ServerEnum
 from redbot.core import commands
 
 class BotTesting(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
     async def cog_check(self, ctx):
         return await _cog_check(ctx, self.bot, ServerEnum.TESTING_SERVER)
 
