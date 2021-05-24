6259bba67aba233c5b334dd79ba7e3e94e8d9fb3
root <cheesycod@outlook.com>
2021-05-24 08:46:39 +0000

    Fix

--- manager/testing.py
+++ manager/testing.py
@@ -17,5 +17,5 @@ class BotTesting(commands.Cog):
         i = 1
         for bot in queue_json["bots"]:
             embed.add_field(name = f"{i}. {bot['username']}#{bot['disc']} ({bot['id']}) - [Invite Bot]({bot['meta']['invite']})", value = "This bot has a status of {Status(bot['status']).__doc__}")
-        embed.set_thumbnail(str(ctx.guild.icon_url))
+        embed.set_thumbnail(url = str(ctx.guild.icon_url))
         return await ctx.send(embed = embed)
