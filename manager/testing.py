7cb741d564b88b7f0fbed89e6d8df0a465295206
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-05-26 08:16:26 +0530

    Update testing.py

--- manager/testing.py
+++ manager/testing.py
@@ -1,4 +1,4 @@
-from .core import _cog_check, _request, _claim_unclaim_requeue, _approve_deny, ServerEnum, Status
+from .core import _cog_check, _request, _queue, _claim_unclaim_requeue, _approve_deny, ServerEnum, Status
 from redbot.core import commands
 from discord import Embed, User, Color
 from http import HTTPStatus
@@ -15,16 +15,7 @@ class BotTesting(commands.Cog):
     @commands.command(aliases=["q", "pending"])
     async def queue(self, ctx):
         """Get all bots in queue"""
-        queue = await _request("GET", ctx, "/api/bots/admin/queue")
-        queue_json = queue[1]
-        embed = Embed(title = "Bots In Queue", description = "These are the bots in the Fates List Queue. Be sure to review them from top to bottom, ignoring Fates List bots")
-        i = 1
-        for bot in queue_json["bots"]:
-            embed.add_field(name = f"{i}. {bot['user']['username']}#{bot['user']['disc']} ({bot['user']['id']})", value = f"This bot has a status of **{Status(bot['user']['status']).__doc__}** and a prefix of **{bot['prefix']}** -> [Invite Bot]({bot['invite']})\n\n**Description:** {bot['description']}\n​")
-            i += 1
-        embed.add_field(name="Credits", value = "skylarr#6666 - For introducing me to redbot and hosting Fates List\nNotDraper#6666 - For helping me through a variety of bugs in the bot")
-        embed.set_thumbnail(url = str(ctx.guild.icon_url))
-        return await ctx.send(embed = embed)
+        return await _queue(ctx)
 
     @commands.command()
     async def claim(self, ctx, bot: User):
