0491c4900ddd962c0a4e7947e406b404c341e2bd
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-05-26 08:15:19 +0530

    Update core.py

--- manager/core.py
+++ manager/core.py
@@ -101,7 +101,20 @@ async def _cog_check(ctx, state: ServerEnum):
         await ctx.send(embed = embed)
         return False
     return True
- 
+
+async def _queue(ctx):
+    queue = await _request("GET", ctx, "/api/bots/admin/queue")
+    queue_json = queue[1]
+    embed = Embed(title = "Bots In Queue", description = "These are the bots in the Fates List Queue. Be sure to review them from top to bottom, ignoring Fates List bots")
+    i = 1
+    for bot in queue_json["bots"]:
+        embed.add_field(name = f"{i}. {bot['user']['username']}#{bot['user']['disc']} ({bot['user']['id']})", value = f"This bot has a status of **{Status(bot['user']['status']).__doc__}** and a prefix of **{bot['prefix']}** -> [Invite Bot]({bot['invite']})\n\n**Description:** {bot['description']}\n​")
+        i += 1
+    embed.add_field(name="Credits", value = "skylarr#6666 - For introducing me to redbot and hosting Fates List\nNotDraper#6666 - For helping me through a variety of bugs in the bot")
+    embed.set_thumbnail(url = str(ctx.guild.icon_url))
+    return await ctx.send(embed = embed)
+
+
 async def _handle(ctx, target: User, op: str, res: dict, succ = "Feel free to relax", kick: Optional[bool] = False):
     opt = op
     if op.endswith("n"):
