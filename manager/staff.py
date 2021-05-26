73b6eb789686d551f6d5d8ba60b531f499c9a257
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-05-26 11:44:54 +0530

    Update staff.py

--- manager/staff.py
+++ manager/staff.py
@@ -3,11 +3,13 @@ from redbot.core import commands
 from discord import Embed, User, Color
 from http import HTTPStatus
 from typing import Optional
+import asyncio
 
 class Staff(commands.Cog):
-    """Commands to handle staff on the staff server"""
+    """Commands to handle the staff server"""
     def __init__(self, bot):
         self.bot = bot
+        self.whitelist = {} # Staff Server Bot Protection
         
     async def cog_check(self, ctx):
         return await _cog_check(ctx, ServerEnum.STAFF_SERVER)
@@ -25,3 +27,34 @@ class Staff(commands.Cog):
     async def unban(self, ctx, bot: User):
         """unban a bot from the list"""
         return await _ban_unban(ctx, bot, "", False)
+
+    @commands.command()
+    async def whitelist(self, ctx, bot: User):
+        staff = await _is_staff(ctx, ctx.author.id, 4)
+        if not staff[0]:
+            return await ctx.send("You cannot temporarily whitelist this member as you are not an admin")
+        self.whitelist[bot.id] = True
+        await ctx.send("Temporarily whitelisted for one minute")
+        await asyncio.sleep(60)
+        try:
+            del self.whitelist[bot.id]
+        except:
+            pass
+        await ctx.send("Unwhitelisted bot again")
+
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        if member.bot:
+            if member.guild.id == main_server:
+                await member.add_roles(member.guild.get_role(self.client.bots_role))
+            elif member.guild.id == test_server:
+                await member.add_roles(member.guild.get_role(test_botsrole))
+            elif not self.whitelist.get(member.id):
+                await member.kick(reason = "Unauthorized Bot")
+            else:
+                del self.whitelist[member.id]
+        else:
+            staff = is_staff(staff_roles, client.get_guild(main_server).get_member(member.id).roles, 2)
+            if staff[0]:
+                await member.add_roles(member.guild.get_role(test_staffrole))
