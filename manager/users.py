404571bc7bc1baf63d87ffea53f6415d9ffe9723
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-05-26 16:48:12 +0530

    Update users.py

--- manager/users.py
+++ manager/users.py
@@ -7,3 +7,13 @@ from typing import Optional
 class User(commands.Cog):
     """Commands made specifically for users to use"""
     
+    @commands.command()
+    async def botdev(self, ctx):
+        """Gives you the Bot Developer role"""
+        res = _request("GET", ctx, f"/api/users/{ctx.author.id}")
+        if res[0] == 404:
+            await ctx.send("You have not even logged in even once on Fates List!")
+            return
+        if not res["bot_developer"]:
+            await ctx.send("You have no eligible bots (your bot is not verified and/or does not belong to you as a owner or extra owner)")
+            return
