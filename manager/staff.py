83072c46cad792087be5512e4426fcd4d8d7172f
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-05-26 08:44:43 +0530

    Update staff.py

--- manager/staff.py
+++ manager/staff.py
@@ -1,4 +1,4 @@
-from .core import ServerEnum, _is_staff, _cog_check, _tokens_missing, _ban_unban
+from .core import ServerEnum, _is_staff, _cog_check, _tokens_missing, _ban_unban, _iamstaff
 from redbot.core import commands
 from discord import Embed, User, Color
 from http import HTTPStatus
@@ -14,23 +14,7 @@ class Staff(commands.Cog):
     
     @commands.command(aliases=["ias", "imstaff", "is"])
     async def iamstaff(self, ctx):
-        staff = await _is_staff(ctx, ctx.author.id, 2)
-        if not staff[0]:
-            try:
-                msg = "You are not a Fates List Staff Member. You will hence be kicked from the staff server!"
-                await ctx.send(msg)
-                await ctx.author.send(msg)
-                await ctx.author.kick()
-            except:
-                await ctx.send("I've failed to kick this member. Staff, please kick this member now!")
-            return
-        servers = await ctx.bot.get_shared_api_tokens("fateslist-si")
-        if not servers.get("ag_role"):
-            await ctx.send(_tokens_missing(["ag_role"]))
-            return
-        staff_ag = int(servers.get("ag_role"))
-        await ctx.author.add_roles( ctx.guild.get_role(staff_ag), ctx.guild.get_role(int(staff[2].staff_id)) )
-        await ctx.send("Welcome home, master!")
+        return await _iamstaff(ctx)
 
     @commands.command()
     async def ban(self, ctx, bot: User, *, reason: Optional[str] = None):
