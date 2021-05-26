ddc66093d37268db7c69368e2467312d8f1f6371
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-05-26 16:39:17 +0530

    Create users.py

---
+++ manager/users.py
@@ -0,0 +1,9 @@
+from .core import _cog_check, _request, ServerEnum, Status
+from redbot.core import commands
+from discord import Embed, User, Color
+from http import HTTPStatus
+from typing import Optional
+
+class User(commands.Cog):
+    """Commands made specifically for users to use"""
+    
