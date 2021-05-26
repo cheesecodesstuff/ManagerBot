13a95e1d21a5d201417629071f685d85539f565f
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-05-26 23:54:23 +0530

    Update __init__.py

--- manager/__init__.py
+++ manager/__init__.py
@@ -1,6 +1,8 @@
 from .testing import BotTesting
 from .staff import Staff
+from .user import User
 
 def setup(bot):
     bot.add_cog(BotTesting(bot))
     bot.add_cog(Staff(bot))
+    bot.add_cog(User(bot))
