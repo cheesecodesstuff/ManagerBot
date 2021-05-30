3a5ec3279567d3fd098676d2581a2863893d84bb
Rootspring <66250972+cheesycod@users.noreply.github.com>
2021-05-30 08:52:35 +0530

    Update core.py

--- manager/core.py
+++ manager/core.py
@@ -256,7 +256,7 @@ async def _certify_uncertify(ctx, bot: User, certify: bool):
     if not bot.bot:
         await ctx.send("That isn't a bot. Please make sure you are pinging a bot or specifying a Bot ID")
         return
-    op = "Certify" if certify else "Uncertofu"
+    op = "Certify" if certify else "Uncertify"
     certify_res = await _request("PATCH", ctx, f"/api/bots/admin/{bot.id}/certify", json = {"mod": str(ctx.author.id), "certify": certify})
     return await _handle(ctx, bot, op, certify_res)
 
